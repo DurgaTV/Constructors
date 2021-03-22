b5eda78df8a88d9c91ac04bf3a73a84056a37d47
DurgaTV <DurgaTV>
2021-03-22 19:36:46 +0100

    new changes

---
+++ src/org/cons/A.java
@@ -0,0 +1,17 @@
+package org.cons;
+
+
+public class A extends B {
+	 public A() {
+      super(12);
+		    System.out.println("Default const...");
+		  }
+		  public static void main(String[] args) {
+		    A a = new A();
+		  }
+
+}
+
+
+
+
---
+++ src/org/cons/B.java
@@ -0,0 +1,9 @@
+package org.cons;
+
+public class B {
+
+	  public B(int id) {
+	    System.out.println(id);
+	  }
+
+	}
